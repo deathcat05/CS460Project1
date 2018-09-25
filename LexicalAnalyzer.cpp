ddcb7d1a9973d963796dd3c13f0252d64dd6e5c4
Catherine Meyer <deathcat05@Catherines-MacBook-Pro.local>
2018-09-24 21:45:39 -0700

    Getters and setters for private variables; having trouble with ostream and istream private variables

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,45 +1,54 @@
 #include <iomanip>
+#include <vector>
 #include <cstdlib>
+#include <fstream>
 #include "LexicalAnalyzer.h"
 
 using namespace std;
 
-static string token_names[] = {	"EOF_T" }; 
+static string token_names[] = {"EOF_T"};
 
-LexicalAnalyzer::LexicalAnalyzer (char * filename)
+LexicalAnalyzer::LexicalAnalyzer(char *filename)
 {
+	GetInputFile(filename);
+
 	// This function will initialize the lexical analyzer class
-	// Transition Table goes here. 
-	
+
+	// Transition Table goes here.
 }
 
-LexicalAnalyzer::~LexicalAnalyzer ()
+LexicalAnalyzer::~LexicalAnalyzer()
 {
 	// This function will complete the execution of the lexical analyzer class
 }
 
-token_type LexicalAnalyzer::GetToken ()
+token_type LexicalAnalyzer::GetToken()
 {
 	// This function will find the next lexeme int the input file and return
 	// the token_type value associated with that lexeme
 	return token;
 }
 
-string LexicalAnalyzer::GetTokenName (token_type t) const
+string LexicalAnalyzer::GetTokenName(token_type t) const
 {
 	// The GetTokenName function returns a string containing the name of the
-	// token passed to it. 
+	// token passed to it.
 	return "";
 }
 
-string LexicalAnalyzer::GetLexeme () const
+string LexicalAnalyzer::GetLexeme() const
 {
-	// This function will return the lexeme found by the most recent call to 
+	// This function will return the lexeme found by the most recent call to
 	// the get_token function
 	return "";
 }
 
-void LexicalAnalyzer::ReportError (const string & msg)
+void LexicalAnalyzer::ReportError(const string &msg)
 {
 	// This function will be called to write an error message to a file
+}
+
+void LexicalAnalyzer::GetInputFile(char *filename)
+{
+	ifstream inFile = &getrmInput();
 }
--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -6,18 +6,46 @@
 
 using namespace std;
 
-enum token_type {NONE = -1, EOF_T, NUM_TOKENS};
-
-class LexicalAnalyzer 
+enum token_type
 {
-    public:
-	LexicalAnalyzer (char * filename);
-	~LexicalAnalyzer ();
-	token_type GetToken ();
-	string GetTokenName (token_type t) const;
-	string GetLexeme () const;
-	void ReportError (const string & msg);
-    private:
+	NONE = -1,
+	EOF_T,
+	NUM_TOKENS
+};
+
+class LexicalAnalyzer
+{
+  public:
+	LexicalAnalyzer(char *filename);
+	~LexicalAnalyzer();
+	token_type GetToken();
+	string GetTokenName(token_type t) const;
+	string GetLexeme() const;
+	void GetInputFile(char *filename); //Reading in the file for manipulation
+	void ReportError(const string &msg);
+
+	//Getters
+	ifstream &getInput() { return input; }
+	ofstream &getListingFile() { return listingFile; }
+	ofstream &getTokenFile() { return tokenFile; }
+	ofstream &getDebugFile() { return debugFile; }
+	//token_type getToken() {return token_type;}
+	string getString() { return line; }
+	int getLinenum() { return linenum; }
+	string getLexeme() { return lexeme; }
+	int getErrors() { return errors; }
+
+	//Setters
+	void setInput(ifstream &_input) { input = _input; }
+	void setListingFile(ofstream &_listingFile) { listingFile = _listingFile; }
+	void setTokenFile(ofstream &_tokenFile) { this->tokenFile = _tokenFile; }
+	void setToken(token_type token) { this->token = token; }
+	void setString(string line) { this->line = line; }
+	void setLinenum(int linenum) { this->linenum = linenum; }
+	void setLexeme(string lexeme) { this->lexeme = lexeme; }
+	void setErrors(int errors) { this->errors = errors; }
+
+  private:
 	ifstream input;
 	ofstream listingFile;
 	ofstream tokenFile;
@@ -29,5 +57,5 @@ class LexicalAnalyzer
 	string lexeme;
 	int errors;
 };
-	
+
 #endif
